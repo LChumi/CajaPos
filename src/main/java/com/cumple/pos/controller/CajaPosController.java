c6c8184826568e2af30f2833d561e7a6c09f425a
Luis Chumi <112025391+LChumi@users.noreply.github.com>
2024-06-10 17:12:19 -0500

    Arreglo de controladores

--- src/main/java/com/cumple/pos/controller/CajaPosController.java
+++ src/main/java/com/cumple/pos/controller/CajaPosController.java
@@ -4,6 +4,8 @@ import com.DF.COM.obj.DatosEnvio;
 import com.DF.COM.obj.DatosRecepcion;
 import com.cumple.pos.service.CajaPosService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -28,5 +30,25 @@ public class CajaPosController {
     @GetMapping("/listaPuertosCom")
     public List<String> listaPuertosCom() {
         return cajaPosService.listaPuerto();
+    }
+
+    @PostMapping("/anular-pago")
+    public ResponseEntity<DatosRecepcion> anularPago(@RequestParam String numReferencia) {
+        DatosRecepcion recepcion = cajaPosService.anularPago(numReferencia);
+        if (recepcion != null) {
+            return ResponseEntity.ok(recepcion);
+        } else {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+    }
+
+    @GetMapping("/ultima-transaccion")
+    public ResponseEntity<DatosRecepcion> obtenerUltimaTransaccion() {
+        DatosRecepcion recepcion = cajaPosService.obtenerUltima();
+        if (recepcion != null) {
+            return ResponseEntity.ok(recepcion);
+        } else {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
     }
 }
