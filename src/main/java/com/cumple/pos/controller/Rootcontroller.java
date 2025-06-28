60812397cc777c82b86b96dd31e2d6a26c7cbe2d
Luis Chumi <112025391+LChumi@users.noreply.github.com>
2025-06-28 09:45:55 -0500

    feat:added root controller

---
+++ src/main/java/com/cumple/pos/controller/Rootcontroller.java
@@ -0,0 +1,19 @@
+package com.cumple.pos.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.view.RedirectView;
+
+@RestController
+public class Rootcontroller {
+
+    @GetMapping("/")
+    public RedirectView info() {
+        return new RedirectView("/actuator/info");
+    }
+
+    @GetMapping("/docs")
+    public RedirectView swaggerUi() {
+        return new RedirectView("/swagger-ui.html");
+    }
+}
