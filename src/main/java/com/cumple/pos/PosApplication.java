05a9ee01ae7a9c7b5365cab52b371f0375967b3a
Luis Chumi <112025391+LChumi@users.noreply.github.com>
2025-07-30 10:06:25 -0500

    feat: added coffaine

--- src/main/java/com/cumple/pos/PosApplication.java
+++ src/main/java/com/cumple/pos/PosApplication.java
@@ -2,8 +2,9 @@ package com.cumple.pos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = LoadBalancerAutoConfiguration.class)
 public class PosApplication {
 
 	public static void main(String[] args) {
