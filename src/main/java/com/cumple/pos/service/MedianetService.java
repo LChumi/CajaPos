a086bbc8708ff241091e8314ab8d9703a482ce51
Luis Chumi <112025391+LChumi@users.noreply.github.com>
2026-01-13 11:26:01 -0500

    refactor: reformat code and delete unused imports

--- src/main/java/com/cumple/pos/service/MedianetService.java
+++ src/main/java/com/cumple/pos/service/MedianetService.java
@@ -1,6 +1,5 @@
 package com.cumple.pos.service;
 
-import cajapinpad.CifradoTramas;
 import cajapinpad.Conexion;
 import cajapinpad.ProccessData;
 import com.cumple.pos.models.DatosEnvioPP;
@@ -18,14 +17,12 @@ public class MedianetService {
     private final CajaPosHelper helper;
     private final Conexion  conexion;
     private final ProccessData processData;
-    private final CifradoTramas cifrado;
 
 
     public MedianetService(){
         this.helper = new CajaPosHelper();
         this.conexion = new Conexion();
         this.processData = ProccessData.getInstance();
-        this.cifrado = new CifradoTramas();
     }
 
     public void enviarLT(double monto, String ipPos, int puertoPos){
@@ -45,19 +42,13 @@ public class MedianetService {
     }
 
     private PagoResponse getConexion(String ipPos, int puertoPos, byte[] datasend) {
-        //conexion.sendData(ipPos, puertoPos, datasend, 30000);
+        conexion.sendData(ipPos, puertoPos, datasend, 30000);
 
-        //byte[] respuestaBytes= conexion.getDataRecived();
+        byte[] respuestaBytes= conexion.getDataRecived();
 
-        //String ascii = processData.hex2AsciiStr(processData.byte2hex(respuestaBytes));
+        String ascii = processData.hex2AsciiStr(processData.byte2hex(respuestaBytes));
 
-
-        System.out.println("----------------------------------------****************************************************---------------------------------------------------------------------------");
-
-        String t2 = "016FPP110002102002001300600003814006900246150060940011600820240718170067060711901200000087170320008200790342400427082501900 AUTORIZACION OK.26002022701648137930XXXX9914340642357B934A11FE68E277BB5131D9EA68AE3084DD7080693707B38830B6A0349DF35020VISA CREDITO        36001050025VISA/ELECTRON            510020752012PAYWAVE/VISA54020A0000000031010 580100000000000590040000600355685799FE42F49E9FF02D5EEC87";
-
-        System.out.println("----- PARSING 1 -----");
-        PagoResponse r2 = parse(t2);
+        PagoResponse r2 = parse(ascii);
         System.out.println(r2);
         return r2;
     }
